9d94bb4551ccda3f54a8a4d63caacd802403ba14
@natalia_gureva <gur.natalia.n@yandex.ru>
2022-09-06 14:45:14 +0500

    колесоотбойники, услуги

--- app/js/app.js
+++ app/js/app.js
@@ -10,3 +10,36 @@ $(document).on('click', '.btn-header.burger', function(event){
   
     event.preventDefault();
   });
+
+  // слайдер//
+  $('.your-class2').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerMode: false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
